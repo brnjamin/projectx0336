8f3dfbf3aa85420edd933c6a2175b4723b3929d4
benjamin <benjamin@algoet.info>
2019-03-04 22:07:40 +0100

    Initial import

---
+++ src/main/java/com/x0336/projectx0336/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.x0336.projectx0336;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
