0cded9c7b0088b9d32da0daae78b85cdef0723b7
benjamin <benjamin@algoet.info>
2019-03-05 11:00:43 +0100

    change-4

--- src/main/java/com/x0336/projectx0336/HelloController.java
+++ src/main/java/com/x0336/projectx0336/HelloController.java
@@ -4,11 +4,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 @RestController
 public class HelloController {
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss.SSSSSS Z");
     @RequestMapping("/")
     public String index() {
-        return "Hello! Greetings from Belgium... (at " + LocalDateTime.now() + ")...";
+        ZonedDateTime now = ZonedDateTime.now();
+        String str = "<html>";
+        str = str + "  Hello! Greetings from Belgium... (at " + now.format(formatter) + ")";
+        str = str + "  <br/>";
+        str = str + "  Default timezone: " + TimeZone.getDefault();
+        str = str + "  <br/>";
+        str = str + "</html>";
+        return str;
     }
 }
